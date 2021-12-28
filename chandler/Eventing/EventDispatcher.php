c4e87066e2b9cc4010d75cf82db89d2fa3a84532
Ilya Bakhlin <git@ilyabakhlin.com>
2021-12-28 22:09:06 +0100

    Testing the Event Dispatcher class.

--- chandler/Eventing/EventDispatcher.php
+++ chandler/Eventing/EventDispatcher.php
@@ -11,22 +11,35 @@ use Chandler\Patterns\TSimpleSingleton;
  */
 class EventDispatcher
 {
+    /**
+     * @var array
+     */
     private $hooks = [];
 
+    /**
+     * @param mixed $hook
+     *
+     * @return bool
+     */
     function addListener($hook): bool
     {
         $this->hooks[] = $hook;
         return true;
     }
 
+    /**
+     * @param \Chandler\Eventing\Events\Event $event
+     *
+     * @return \Chandler\Eventing\Events\Event
+     */
     function pushEvent(Events\Event $event): Events\Event
     {
-        foreach ($hooks as $hook) {
+        foreach ($this->hooks as $hook) {
             if ($event instanceof Events\Cancelable)
                 if ($event->isCancelled())
                     break;
             $method = "on" . str_replace("Event", "", get_class($event));
-            if (!method_exists($hook, $methodName)) continue;
+            if (!method_exists($hook, $method)) continue;
             $hook->$method($event);
         }
         return $event;
