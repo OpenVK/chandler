d7d97fc4bf73a1ff87aec52e9938eb63e871cb73
Ilya Bakhlin <git@ilyabakhlin.com>
2021-12-28 19:46:12 +0100

    Reformatting the Cancelable interface.

--- chandler/Eventing/Events/Cancelable.php
+++ chandler/Eventing/Events/Cancelable.php
@@ -1,11 +1,17 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types = 1);
+
 namespace Chandler\Eventing\Events;
 
+/**
+ * @package Chandler\Eventing\Events
+ */
 interface Cancelable
 {
     protected $cancelled;
-    
+
     function cancel(): void;
-    
+
     function isCancelled(): bool;
 }
