104416c967673dda10ae39b8529a2d5239eb8c56
Ilya Bakhlin <git@ilyabakhlin.com>
2022-01-09 00:37:58 +0100

    Removing the Bootstrap class.

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use Chandler\MVC\Routing\Router;
+
 require_once(dirname(__DIR__) . "/vendor/autoload.php");
 
 define("CHANDLER_ROOT", dirname(__DIR__));
@@ -9,6 +11,13 @@ define("CHANDLER_ROOT_CONF", yaml_parse_file(CHANDLER_ROOT . "/chandler.yml"));
 define("CHANDLER_EXTENSIONS_AVAILABLE", CHANDLER_ROOT . CHANDLER_ROOT_CONF["extensions"]["available"]);
 define("CHANDLER_EXTENSIONS_ENABLED", CHANDLER_ROOT . CHANDLER_ROOT_CONF["extensions"]["enabled"]);
 
-$bootstrap = new Bootstrap();
+Chandler\Extensions\ExtensionManager::getInstance();
 
-$bootstrap->ignite();
+ob_start();
+if (($output = Router::getInstance()->execute($_SERVER["REQUEST_URI"])) !== null)
+    echo $output;
+else
+    chandler_http_panic(404, "Not Found", "No routes for {$_SERVER["REQUEST_URI"]}.");
+ob_flush();
+ob_end_flush();
+flush();
